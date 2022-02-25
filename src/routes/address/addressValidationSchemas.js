8640baccc74e4dcb674f73e22d9b2f877839cebc
Matheus Martino <matheus.martino97@gmail.com>
2022-02-25 01:36:51 -0300

    Move zipCode pattern validation to routes layer.

--- src/routes/address/addressValidationSchemas.js
+++ src/routes/address/addressValidationSchemas.js
@@ -1,10 +1,11 @@
 import Joi from 'joi';
+import { zipCodePattern } from '../../constants/addressContants';
 
 export const createByZipCodeValidationSchema = {
   body: Joi.object({
     street: Joi.string(),
     number: Joi.string().required(),
-    zipCode: Joi.string().required(),
+    zipCode: Joi.string().regex(zipCodePattern).required(),
     notes: Joi.string(),
   }),
 };
